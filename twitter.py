33d7878d115b61b888fc284d4f63b92b67784f31
odylomv <odylomv@gmail.com>
2022-08-24 13:01:34 +0300

    cleaned up code and fixed old rules persisting

--- twitter.py
+++ twitter.py
@@ -13,25 +13,17 @@ auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_
 image_uploader = tweepy.API(auth)
 
 client = tweepy.Client(
-    consumer_key=API_KEY,
-    consumer_secret=API_SECRET,
-    access_token=ACCESS_TOKEN,
-    access_token_secret=ACCESS_TOKEN_SECRET
+    consumer_key=API_KEY, consumer_secret=API_SECRET,
+    access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET
 )
-
-
-def print_response(response):
-    print(response.data.data)
-    tweet_secret = get_secret(response.includes)
-    post_response(response.data.data['id'], tweet_secret)
 
 
 def get_secret(includes):
     url = includes['media'][0].url
     response = requests.get(url)
 
-    with open(TEMP_IMAGE_PATH, 'wb') as img:
-        img.write(response.content)
+    with open(TEMP_IMAGE_PATH, 'wb') as file:
+        file.write(response.content)
     return reveal(TEMP_IMAGE_PATH)
 
 
@@ -43,7 +35,26 @@ def post_response(tweet_id, tweet_secret):
     client.create_tweet(text='Hi there!', media_ids=[media.media_id], in_reply_to_tweet_id=tweet_id, user_auth=True)
 
 
-streaming_client = tweepy.StreamingClient(BEARER_TOKEN)
+class BitcoinStream(tweepy.StreamingClient):
+    def __init__(self, bearer_token):
+        super().__init__(bearer_token)
+        self.clean_rules()
+
+    def clean_rules(self):
+        rule_ids = [rule.id for rule in self.get_rules().data]
+
+        if len(rule_ids) > 0:
+            self.delete_rules(rule_ids)
+            print('Successfully deleted old rules')
+        else:
+            print("No rules to delete")
+
+    def on_response(self, response):
+        print(response.data.data)
+        tweet_secret = get_secret(response.includes)
+        post_response(response.data.data['id'], tweet_secret)
+
+
+streaming_client = BitcoinStream(BEARER_TOKEN)
+streaming_client.add_rules(tweepy.StreamRule('#lomvardoBTC has:media -is:reply'))
 streaming_client.filter(media_fields=['url'], expansions=['attachments.media_keys'])
-streaming_client.add_rules(tweepy.StreamRule('#lomvardoBTC'))
-streaming_client.on_response = print_response
